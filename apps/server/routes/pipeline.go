917bf096edd66cd9543b8d88126ab662c789859a
saradab-mindfire <saradab@mindfiresolutions.com>
2026-07-08 01:06:54 +0530

    fix: parsed param id and added api url as constant

--- apps/server/routes/pipeline.go
+++ apps/server/routes/pipeline.go
@@ -6,12 +6,14 @@ import (
 	"github.com/saradab-mindfire/data-processing-pipeline/apps/server/middleware"
 )
 
+const apiV1Prefix = "/api/v1"
+
 func SetupRoutes(router *gin.Engine, apiKey string) {
 	router.Use(middleware.RequireAPIKey(apiKey))
 
 	router.Static("/exports", "exports")
 
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(apiV1Prefix)
 	pipelines := v1.Group("/pipelines")
 	{
 		pipelines.POST("", controllers.CreatePipeline)
